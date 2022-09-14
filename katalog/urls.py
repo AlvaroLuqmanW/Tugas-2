d9a687f1d2b10a029cf560b33aa6a0e7d1c5ff01
alvaro.luqman1 <alvaro.luqman@gmail.com>
2022-09-14 16:57:29 +0700

    changes

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from katalog.views import katalog
+from katalog.views import show_katalog
 
 app_name = 'katalog'
 
 urlpatterns = [
-    path('', katalog, name='katalog'),
+    path('', show_katalog, name='show_katalog'),
    
 ]
--- katalog/views.py
+++ katalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # TODO: Create your views here.
-def katalog(request):
+def show_katalog(request):
     return render(request, 'katalog.html')
