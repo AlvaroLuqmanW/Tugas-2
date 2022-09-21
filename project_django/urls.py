f08ae3ecc86586036e467c7fcc3a43d14d4bcc8b
alvaro.luqman1 <alvaro.luqman@gmail.com>
2022-09-21 16:20:13 +0700

    Tugas 3

--- project_django/urls.py
+++ project_django/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),  
-    path('', include('mywatchlist.urls')),
+    path('mywatchlist/', include('mywatchlist.urls')),
 ]
 
 # TODO: Implement Routings Here
