8042017e4d9dde2d7699c5f43f86e9da15cebe40
alvaro.luqman1 <alvaro.luqman@gmail.com>
2022-09-28 10:01:37 +0700

    Fix Tugas 2

--- project_django/urls.py
+++ project_django/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),  
     path('mywatchlist/', include('mywatchlist.urls')),
+    path('katalog/', include('katalog.urls'))
 ]
 
 # TODO: Implement Routings Here
